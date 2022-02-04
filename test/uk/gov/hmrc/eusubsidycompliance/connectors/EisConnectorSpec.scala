a084190f609d9bcf1d25522d4f3fd3d3c9b9c880
Carwyn Ellis <carwynellis@gmail.com>
2022-02-04 15:40:58 +0000

    ESC-230 complete tests for retrieveUndertaking

--- test/uk/gov/hmrc/eusubsidycompliance/connectors/EisConnectorSpec.scala
+++ test/uk/gov/hmrc/eusubsidycompliance/connectors/EisConnectorSpec.scala
@@ -23,40 +23,111 @@ import org.scalatest.wordspec.AnyWordSpecLike
 import play.api.Application
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.test.Helpers.running
+import uk.gov.hmrc.eusubsidycompliance.models.{BusinessEntity, Undertaking}
 import uk.gov.hmrc.eusubsidycompliance.models.json.digital.EisBadResponseException
-import uk.gov.hmrc.eusubsidycompliance.models.types.EORI
+import uk.gov.hmrc.eusubsidycompliance.models.types.{EORI, IndustrySectorLimit, Sector, UndertakingName, UndertakingRef}
 import uk.gov.hmrc.eusubsidycompliance.testutil.WiremockSupport
-import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.http.{HeaderCarrier, UpstreamErrorResponse}
 
-import java.time.Instant
+import java.time.{Instant, ZoneId}
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class EisConnectorSpec extends AnyWordSpecLike with Matchers with WiremockSupport with ScalaFutures
   with IntegrationPatience {
 
-  implicit private lazy val hc: HeaderCarrier = HeaderCarrier()
+  private val RetrieveUndertakingPath = "/scp/retrieveundertaking/v1"
+  private val eori = EORI("GB123456789012")
+  private val now = Instant.now()
+
+  implicit private val hc: HeaderCarrier = HeaderCarrier()
 
   "EisConnector" when {
 
     "retrieveUnderTaking is called" should {
 
+      "return an UnderTaking for a successful request" in {
+        val app = configuredApplication
+
+        val reference = UndertakingRef("SomeReference")
+        val name = UndertakingName("SomeName")
+        val sector = Sector.other
+        val industrySectorLimit = IndustrySectorLimit(BigDecimal(200000.00))
+        val lastSubUsageDate = now.atZone(ZoneId.of("Europe/London")).toLocalDate
+
+        val expectedUndertaking = Undertaking(
+          Some(reference),
+          name,
+          sector,
+          Some(industrySectorLimit),
+          Some(lastSubUsageDate),
+          List(BusinessEntity(eori, leadEORI = true, None))
+        )
+
+        givenEisReturns(200, RetrieveUndertakingPath,
+          s"""{
+             | "retrieveUndertakingResponse": {
+             |   "responseCommon": {
+             |     "status": "OK",
+             |     "processingDate": "$now"
+             |   },
+             |   "responseDetail": {
+             |      "undertakingReference": "$reference",
+             |      "undertakingName": "$name",
+             |      "industrySector": "$sector",
+             |      "industrySectorLimit": $industrySectorLimit,
+             |      "lastSubsidyUsageUpdt": "$lastSubUsageDate",
+             |      "undertakingBusinessEntity": [{
+             |        "businessEntityIdentifier": "$eori",
+             |        "leadEORI": true
+             |      }]
+             |   }
+             | }
+             |}""".stripMargin
+        )
+
+        running(app) {
+          val underTest = app.injector.instanceOf[EisConnector]
+          underTest.retrieveUndertaking(EORI("GB123456789012")).futureValue mustBe expectedUndertaking
+        }
+
+      }
+
+      "return a 404 if a NOT_OK response is received with the not found error code" in {
+        val app = configuredApplication
+
+        givenEisReturns(200, RetrieveUndertakingPath,
+          s"""{
+             | "retrieveUndertakingResponse": {
+             |   "responseCommon": {
+             |     "status": "NOT_OK",
+             |     "processingDate": "$now",
+             |     "returnParameters": [{
+             |       "paramName": "ERRORCODE",
+             |       "paramValue": "107"
+             |     }]
+             |   }
+             | }
+             |}""".stripMargin
+        )
+
+        running(app) {
+          val underTest = app.injector.instanceOf[EisConnector]
+          underTest.retrieveUndertaking(EORI("GB123456789012")).failed.futureValue mustBe a[UpstreamErrorResponse]
+        }
+      }
+
       "throw an EisBadResponseException if the response has status NOT_OK" in {
         val app = configuredApplication
 
-        val now = Instant.now()
-
-        server.stubFor(
-          post(urlEqualTo("/scp/retrieveundertaking/v1"))
-            .willReturn(ok(
-              s"""{
-                 | "retrieveUndertakingResponse": {
-                 |   "responseCommon": {
-                 |     "status": "NOT_OK",
-                 |     "processingDate": "$now"
-                 |   }
-                 | }
-                 |}""".stripMargin
-            ))
+        givenEisReturns(200, RetrieveUndertakingPath,
+          s"""{
+             | "retrieveUndertakingResponse": {
+             |   "responseCommon": {
+             |     "status": "NOT_OK",
+             |     "processingDate": "$now"
+             |   }
+             | }
+             |}""".stripMargin
         )
 
         running(app) {
@@ -75,5 +146,13 @@ class EisConnectorSpec extends AnyWordSpecLike with Matchers with WiremockSuppor
       "microservice.services.eis.host" -> "localhost",
       "microservice.services.eis.port" -> server.port(),
   ).build()
+
+  private def givenEisReturns(status: Int, url: String, body: String) =
+    server.stubFor(
+      post(urlEqualTo(url))
+        .willReturn(aResponse()
+          .withStatus(status)
+          .withBody(body)
+      ))
 
 }
