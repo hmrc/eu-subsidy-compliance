006d217cb1a807f41115ee5fcde8150b4828755d
Carwyn Ellis <carwynellis@gmail.com>
2022-02-08 16:24:27 +0000

    ESC-230 update project deps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,14 +7,14 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.20.0",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.53.0",
-    "org.typelevel"           %% "cats-core"                  % "2.6.1",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.59.0",
+    "org.typelevel"           %% "cats-core"                  % "2.7.0",
     "com.chuusai"             %% "shapeless"                  % "2.3.7"
   )
 
   val test = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "5.20.0"   % Test,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % "0.53.0"   % Test,
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % "0.59.0"   % Test,
     "com.vladsch.flexmark"    %  "flexmark-all"               % "0.36.8"   % "test, it",
     // TODO - remove mockito once all tests transitioned to scalamock
     "org.mockito"             %  "mockito-core"               % "3.9.0"    % Test,
