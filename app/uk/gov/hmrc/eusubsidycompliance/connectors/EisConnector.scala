c356f8153b3b8a0465fce38079b582cf87c0d797
Patrick Byrne <patrick.byrne@digital.hmrc.gov.uk>
2023-06-23 10:01:23 +0100

    Add EORI to log for 107 error (#56)

--- app/uk/gov/hmrc/eusubsidycompliance/connectors/EisConnector.scala
+++ app/uk/gov/hmrc/eusubsidycompliance/connectors/EisConnector.scala
@@ -69,7 +69,7 @@ class EisConnector @Inject() (
       .recover {
         case e: EisBadResponseException if e.code == EisParamValue("107") =>
           logger.info(s"No undertaking found for $eori")
-          Left(ConnectorError(NOT_FOUND, s"Undertaking reference in the API not subscribed in ETMP."))
+          Left(ConnectorError(NOT_FOUND, s"Undertaking reference EORI:$eori in the API not subscribed in ETMP."))
 
         case e: EisBadResponseException if e.code == EisParamValue("055") =>
           logger.info(s" Eori : $eori does not exist in ETMP")
