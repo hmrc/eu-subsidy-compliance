6ac38fa0e871010707e45cc53c7d52c677cbeecf
lewis-nicholls <173899555+lewis-nicholls@users.noreply.github.com>
2025-03-21 12:57:25 +0000

    Dependency upgrades (#95)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,14 +2,14 @@ import sbt._
 
 object AppDependencies {
 
-  val bootStrapVersion = "9.7.0"
-  val hmrcMongoVersion = "2.4.0"
+  val bootStrapVersion = "9.11.0"
+  val hmrcMongoVersion = "2.6.0"
 
 
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30"    % bootStrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"           % hmrcMongoVersion,
-    "uk.gov.hmrc"       %% "internal-auth-client-play-30" % "3.0.0",
+    "uk.gov.hmrc"       %% "internal-auth-client-play-30" % "3.1.0",
     "org.typelevel"     %% "cats-core"                    % "2.13.0",
     "com.chuusai"       %% "shapeless"                    % "2.3.12"
   )
